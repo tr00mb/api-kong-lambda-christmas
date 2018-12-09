2bf628936135a230c4307e21056258280ab9cdd0
devopscloudnext <devopscloudnext@gmail.com>
2018-12-09 11:25:07 +0100

    add lambda 3: getTotalPrice

--- lambda-functions/getTotalPrice/getTotalPrice.py
+++ lambda-functions/getTotalPrice/getTotalPrice.py
@@ -20,10 +20,29 @@ dynamodb = boto3.resource('dynamodb',DYNAMODB_REGION)
 #from boto import module lambda
 client = boto3.client('lambda')
 
+#Get ARN of lambda to call
+invoked_function_arn = ""
 
+#Event to send to invoke lambda getShippingPrice
+event_to_send =""
 
 def lambda_handler(event, context):
+    
+    #prix du panier transmis en parametre et recupéré dans l'event
+    prix_panier = event['price_of_shopping_cart']
+
+    #invocation de la lambda qui renvoit le prix 
+    called_function = context.invoked_function_arn
     response = client.invoke(
-        FunctionName='LAMBDA_TO_INVOKE',
-        InvocationType='Event'
+        FunctionName=called_function,
+        InvocationType='RequestResponse',
+        Payload=bytes(json.dumps(event_to_send))
     )
+    data = response['Payload'].read()
+    data = json.loads(response['Payload'].read().decode())
+    shippingPrice = data ['shipping_price']
+
+    #somme des deux prix
+    Total_price = shippingPrice + prix_panier
+    return Total_price
+
