e060aae442216a651bfb6fb0cc0ffdb98dc4278a
Yohann Agostini <yohann@yohannmac.fr.devoteam.com>
2018-12-10 12:49:25 +0100

    correct imports in getAllProducts

--- lambda-functions/getAllProducts/getAllProducts.py
+++ lambda-functions/getAllProducts/getAllProducts.py
@@ -2,30 +2,30 @@ import os
 import sys
 import json
 import boto3
-import botocore
 import logging
 import uuid
 from boto3.dynamodb.conditions import Key, Attr
-from boto.s3.key import Key
+
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-#DynamodDB table name and region 
+#DynamodDB table name and region
 DYNAMODB_REGION = "eu-west-3"
-DYNAMODB_TABLE_NAME ="api_days_calcul_distance"
+DYNAMODB_TABLE_NAME ="kong"
 
 os.environ["AWS_DEFAULT_REGION"] = "eu-west-3"
 dynamodb = boto3.resource('dynamodb',DYNAMODB_REGION)
 
-#Bucket s3 
+#Bucket s3
 BUCKET_NAME = 'my-bucket' # image bucket name
 KEY = 'my_image_in_s3.jpg' # r object key
 s3 = boto3.resource('s3')
 
 def lambda_handler(event, context):
-    scan_table(DYNAMODB_TABLE_NAME,None,None)
-   
+
+    return scan_table(DYNAMODB_TABLE_NAME,None,None)
+
 
 def scan_table(table_name, filter_key=None, filter_value=None):
     """
@@ -40,7 +40,7 @@ def scan_table(table_name, filter_key=None, filter_value=None):
         #query_s3_for_image(response)
     else:
         response = table.scan()
-        #query_s3_for_image(response)   
+        #query_s3_for_image(response)
     print(response)
     return response
 
